fb6154ef03209d38ce5a875ccfd268d1a79e2270
cpiggott <cpiggott@ksu.edu>
2015-08-07 00:13:53 -0500

    Added action, moving to mac

--- MyApplication/app/src/main/java/co/acrossed/android/HomeFragment.java
+++ MyApplication/app/src/main/java/co/acrossed/android/HomeFragment.java
@@ -2,27 +2,36 @@ package co.acrossed.android;
 
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -54,6 +63,9 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         //Pull all the tasks
 
+        setHasOptionsMenu(true);
+
+
 
 
 
@@ -75,16 +87,19 @@ public class HomeFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //@TODO: Open a dialog with info
+                String t = "sd";
+
             }
         });
 
         ParseQuery<Task> query = Task.getQuery();
-        query.whereEqualTo(ParseConsts.Task.IsArchived, false).whereEqualTo(ParseConsts.Task.User, ParseUser.getCurrentUser());
+        query.whereEqualTo(ParseConsts.Task.IsComplete, false).whereEqualTo(ParseConsts.Task.User, ParseUser.getCurrentUser());
 
         query.findInBackground(new FindCallback<Task>() {
             @Override
             public void done(List<Task> list, ParseException e) {
                 if (e == null && list.size() > 0) {
+                    Collections.sort(list, new CategoryComparotor());
                     tasks = list;
                     taskAdapter.addAll(tasks);
                     taskAdapter.notifyDataSetChanged();
@@ -100,10 +115,41 @@ public class HomeFragment extends Fragment {
         super.onAttach(activity);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.home, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if (item.getItemId() == R.id.action_add) {
+            //Bodeefit.makeToastShort("Finish workout");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 
 
+
+
+    public class CategoryComparotor implements Comparator {
+
+
+        @Override
+        public int compare(Object o, Object t1) {
+            String task1= ((Task)o).getCategory();
+            String task2 = ((Task)t1).getCategory();
+
+            // ascending order (descending order would be: task2.compareTo(name1))
+            return task1.compareTo(task2);
+        }
+    }
 
 
 
@@ -120,7 +166,7 @@ public class HomeFragment extends Fragment {
         @Override
         public View getView(int position, View convertView, ViewGroup parent){
             final ViewHolder viewHolder;
-            Task task = getItem(position);
+            final Task task = getItem(position);
 
 
             if(task instanceof Category){
@@ -140,6 +186,38 @@ public class HomeFragment extends Fragment {
                 viewHolder = new ViewHolder();
                 viewHolder.tvTaskName = (TextView) convertView.findViewById(R.id.textViewTaskTitle);
                 viewHolder.checkBoxTask = (CheckBox) convertView.findViewById(R.id.checkBoxTask);
+                viewHolder.tvCategory = (TextView) convertView.findViewById(R.id.textViewCategoryT);
+                viewHolder.rlLayout = (RelativeLayout) convertView.findViewById(R.id.layoutRelative);
+
+                viewHolder.rlLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //                Task currentTask = tasks.get(i);
+
+
+                final Dialog dialog = new Dialog(getActivity());
+                        dialog.setContentView(R.layout.custom_dialog);
+
+                TextView description = (TextView) dialog.findViewById(R.id.textViewDescriptionDialog);
+                description.setText(task.getDescription());
+
+                TextView category = (TextView) dialog.findViewById(R.id.textViewCategoryDialog);
+                category.setText(task.getCategory());
+
+                TextView title = (TextView) dialog.findViewById(R.id.textViewTitleDialog);
+                title.setText(task.getTaskName());
+
+                Button okButton = (Button) dialog.findViewById(R.id.buttonOkDialog);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+                    }
+                });
 
 
 
@@ -148,17 +226,33 @@ public class HomeFragment extends Fragment {
                     public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                         if (isChecked) {
                             //change everything so that it is complete
-                            viewHolder.tvTaskName.setPaintFlags(viewHolder.tvTaskName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                            viewHolder.tvTaskName.setTextColor(getResources().getColor(R.color.gray));
+                            task.setIsComplete(true);
+                            task.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    viewHolder.tvTaskName.setPaintFlags(viewHolder.tvTaskName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                                    viewHolder.tvTaskName.setTextColor(getResources().getColor(R.color.gray));
+                                }
+                            });
+
                         } else {
                             //change everything so that it is not complete
-                            viewHolder.tvTaskName.setPaintFlags((viewHolder.tvTaskName.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG));
-                            viewHolder.tvTaskName.setTextColor(getResources().getColor(R.color.black));
+                            task.setIsComplete(false);
+                            task.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    viewHolder.tvTaskName.setPaintFlags((viewHolder.tvTaskName.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG));
+                                    viewHolder.tvTaskName.setTextColor(getResources().getColor(R.color.black));
+                                }
+                            });
+
                         }
                     }
                 });
 
                 viewHolder.tvTaskName.setText(task.getTaskName());
+                viewHolder.tvCategory.setText(task.getCategory());
+
                 if(task.getIsComplete()){
                     viewHolder.tvTaskName.setPaintFlags(viewHolder.tvTaskName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
                     viewHolder.tvTaskName.setTextColor(getResources().getColor(R.color.gray));
@@ -180,6 +274,8 @@ public class HomeFragment extends Fragment {
         private class ViewHolder {
             CheckBox checkBoxTask;
             TextView tvTaskName;
+            TextView tvCategory;
+            RelativeLayout rlLayout;
         }
 
 
